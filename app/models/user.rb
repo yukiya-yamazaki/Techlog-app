028385f1d12dc9eabe573b35a0ca981f57400a60
yukiya-yamazaki <yuki0408.y@outlook.jp>
2026-05-14 21:53:53 +0900

    User.nicknameのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, length: { maximum: 20 }
+  validates :nickname, presence: true,length: { maximum: 20 }
 end
