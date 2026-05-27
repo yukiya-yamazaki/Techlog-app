dd818dd40ba2c815e850fa7eca875f84cd2cbe9a
yukiya-yamazaki <yuki0408.y@outlook.jp>
2026-05-27 21:56:01 +0900

    学習ログ削除機能を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,12 @@ class PostsController < ApplicationController
   end
  
   def destroy
+    post = Post.find_by(id: params[:id]) # URLの:idに対応する投稿を取得
+    if post.user == current_user # 投稿のユーザと現在のユーザが同じか確認
+      post.destroy # 投稿を削除
+      flash[:notice] = '投稿が削除されました' # 成功時のフラッシュメッセージ
+    end
+    redirect_to posts_path
   end
  
   private
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
- 
+
 describe 'Post', type: :system do
   before do
     driven_by :rack_test
     @user = create(:user) # ログイン用ユーザー作成
-    @post = create(:post, title: 'RSpec学習完了', content: 'System Specを作成した', user_id: @user.id) # 投稿用のPost作成
+    @post = create(:post, title: 'RSpec学習完了', content: 'System Specを作成した', user_id: @user.id)
   end
- 
+
   # 投稿フォーム
   let(:title) { 'テストタイトル' }
   let(:content) { 'テスト本文' }
- 
+
   describe 'ログ投稿機能の検証' do
     # ログ投稿を行う一連の操作を subject にまとめる
     subject do
@@ -18,7 +18,7 @@ describe 'Post', type: :system do
       fill_in 'post_content', with: content
       click_button 'ログを記録'
     end
- 
+
     context 'ログインしていない場合' do
       before { visit '/posts/new' }
       it 'ログインページへリダイレクトする' do
@@ -26,7 +26,7 @@ describe 'Post', type: :system do
         expect(page).to have_content('ログインしてください。')
       end
     end
- 
+
     context 'ログインしている場合' do
       before do
         sign_in @user
@@ -35,7 +35,7 @@ describe 'Post', type: :system do
       it 'ログインページへリダイレクトしない' do
         expect(current_path).not_to eq('/users/sign_in')
       end
- 
+
       context 'パラメータが正常な場合' do
         it 'Postを作成できる' do
           expect { subject }.to change(Post, :count).by(1)
@@ -43,14 +43,14 @@ describe 'Post', type: :system do
           expect(page).to have_content('投稿しました')
         end
       end
- 
+
       context 'パラメータが異常な場合' do
         let(:title) { nil }
         it 'Postを作成できない' do
           expect { subject }.not_to change(Post, :count)
           expect(page).to have_content('投稿に失敗しました')
         end
- 
+
         it '入力していた内容は維持される' do
           subject
           expect(page).to have_field('post_content', with: content)
@@ -61,7 +61,7 @@ describe 'Post', type: :system do
 
   describe 'ログ詳細機能の検証' do
     before { visit "/posts/#{@post.id}" }
- 
+
     it 'Postの詳細が表示される' do
       expect(page).to have_content('RSpec学習完了')
       expect(page).to have_content('System Specを作成した')
@@ -70,27 +70,63 @@ describe 'Post', type: :system do
   end
 
   describe 'ログ一覧機能の検証' do
-  before do
-    # 事前にもう一つの投稿を作成
-    @post2 = create(:post, title: 'RSpec学習完了 2', content: 'System Specを作成した 2', user_id: @user.id)
-    visit '/posts'
+    before do
+      # 事前にもう一つの投稿を作成
+      @post2 = create(:post, title: 'RSpec学習完了 2', content: 'System Specを作成した 2', user_id: @user.id)
+      visit '/posts'
+    end
+
+    it '1件目のPostの詳細が表示される' do
+      expect(page).to have_content('RSpec学習完了')
+      expect(page).to have_content('System Specを作成した')
+      expect(page).to have_content(@user.nickname)
+    end
+
+    it '2件目のPostの詳細が表示される' do
+      expect(page).to have_content('RSpec学習完了 2')
+      expect(page).to have_content('System Specを作成した 2')
+      expect(page).to have_content(@user.nickname)
+    end
+
+    it '投稿タイトルをクリックすると詳細ページへ遷移する' do
+      click_link 'RSpec学習完了'
+      expect(current_path).to eq("/posts/#{@post.id}")
+    end
   end
- 
-  it '1件目のPostの詳細が表示される' do
-    expect(page).to have_content('RSpec学習完了')
-    expect(page).to have_content('System Specを作成した')
-    expect(page).to have_content(@user.nickname)
+
+  describe 'ログ削除機能の検証' do
+    context '投稿したユーザーでログインしている場合' do
+      before do
+        sign_in @user # テストユーザーでログイン
+        visit "/posts/#{@post.id}" # 投稿詳細ページにアクセス
+      end
+
+      it '削除ボタンを表示する' do
+        expect(page).to have_button('削除') # 削除ボタンが表示されていることを確認
+      end
+
+      it '削除ボタンをクリックすると削除できる' do
+        expect do
+          click_button '削除'
+        end.to change(Post, :count).by(-1) # 削除ボタンをクリックすると投稿が1件減ることを確認
+
+        expect(current_path).to eq('/posts') # 投稿一覧ページにリダイレクトされていることを確認
+        expect(page).to have_content('投稿が削除されました') # 削除完了メッセージが表示されていることを確認
+        expect(page).not_to have_content('RSpec学習完了') # 削除した投稿が一覧に表示されていないことを確認
+      end
+    end
+
+    context '投稿したユーザーでログインしていない場合' do
+      it '削除ボタンを表示しない' do
+        visit "/posts/#{@post.id}" # 投稿詳細ページにアクセス
+        expect(page).not_to have_button('削除') # 削除ボタンが表示されていないことを確認
+      end
+
+      it '直接リクエストを投げても削除されない' do
+        expect do
+          delete post_path(@post) # DELETE リクエストを直接送信
+        end.not_to change(Post, :count) # 投稿レコードの数が変わらないことを確認
+      end
+    end
   end
- 
-  it '2件目のPostの詳細が表示される' do
-    expect(page).to have_content('RSpec学習完了 2')
-    expect(page).to have_content('System Specを作成した 2')
-    expect(page).to have_content(@user.nickname)
-  end
- 
-  it '投稿タイトルをクリックすると詳細ページへ遷移する' do
-    click_link 'RSpec学習完了'
-    expect(current_path).to eq("/posts/#{@post.id}")
-  end
-end
 end
